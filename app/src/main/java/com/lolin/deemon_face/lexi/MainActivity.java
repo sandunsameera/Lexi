8d36c2d909a765bb4c5d3f628f8c939b0cf7805c
sandunsameera <sandunsameera25@gmail.com>
2019-01-13 19:59:12 +0530

    First activity

--- app/src/main/java/com/lolin/deemon_face/lexi/MainActivity.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/MainActivity.java
@@ -1,13 +1,27 @@
 package com.lolin.deemon_face.lexi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button MainBtnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        MainBtnSignIn = findViewById (R.id.main_btn_signIn);
+        MainBtnSignIn.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity.this,menuActivity.class);
+                startActivity (intent);
+            }
+        });
     }
 }
