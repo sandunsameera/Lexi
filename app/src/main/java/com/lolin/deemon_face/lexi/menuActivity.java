bf8d05bc9182bde396b40edf41195ffe4dd3b8a4
sandunsameera <sandunsameera25@gmail.com>
2019-01-13 21:39:20 +0530

    UI update

--- app/src/main/java/com/lolin/deemon_face/lexi/menuActivity.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/menuActivity.java
@@ -1,13 +1,32 @@
 package com.lolin.deemon_face.lexi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class menuActivity extends AppCompatActivity {
+
+    private Button menuBtnWedding;
+    private Button menuBtnBirthday;
+    private Button menuBtnBeachParty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_menu);
+
+        menuBtnWedding = findViewById (R.id.menu_btn_Wedding);
+        menuBtnBirthday = findViewById (R.id.menu_btn_Birthday);
+        menuBtnBeachParty = findViewById (R.id.menu_btn_beachParty);
+
+        Intent intent1 = new Intent (menuActivity.this,weddingActivity.class);
+        startActivity (intent1);
+
+        Intent intent2 = new Intent (menuActivity.this,bithdayActivity.class);
+        startActivity (intent2);
+
+        Intent intent3 = new Intent (menuActivity.this,beachParty.class);
+        startActivity (intent3);
     }
 }
