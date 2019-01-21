6d2a4cce57d4c25a7817698c148b1d9675ca997a
sandunsameera <sandunsameera25@gmail.com>
2019-01-21 16:16:09 +0530

    making sign in with firebase google sign In authentication

--- app/src/main/java/com/lolin/deemon_face/lexi/MainActivity.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/MainActivity.java
@@ -1,13 +1,11 @@
 package com.lolin.deemon_face.lexi;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -46,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance ();
         googleBtn = findViewById (R.id.main_Btn_googleSignIn);
 
+
+
         googleBtn.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View v) {
@@ -68,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
 
