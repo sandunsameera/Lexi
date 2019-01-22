40fe39b8e3dfa78fb0c2cba161fbb7cd0d57c987
sandunsameera <sandunsameera25@gmail.com>
2019-01-22 18:19:47 +0530

    making sign in with firebase google sign In authentication

--- app/src/main/java/com/lolin/deemon_face/lexi/MainActivity.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/MainActivity.java
@@ -62,11 +62,11 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
+
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
--- app/src/main/java/com/lolin/deemon_face/lexi/menuActivity.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/menuActivity.java
@@ -1,25 +1,59 @@
 package com.lolin.deemon_face.lexi;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class menuActivity extends AppCompatActivity {
 
     public Button menuBtnWedding;
     public Button menuBtnBirthday;
     public Button menuBtnBeachParty;
+    public Button logOutBtn;
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+
+    @Override
+    protected void onStart() {
+        super.onStart ();
+        mAuth.addAuthStateListener (mAuthListner);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_menu);
 
+        logOutBtn = findViewById (R.id.menu_btn_logout);
         menuBtnWedding = findViewById (R.id.menu_btn_Wedding);
         menuBtnBirthday = findViewById (R.id.menu_btn_Birthday);
         menuBtnBeachParty = findViewById (R.id.menu_btn_beachParty);
+
+        mAuth = FirebaseAuth.getInstance ();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener () {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser () == null){
+                    startActivity (new Intent (menuActivity.this,MainActivity.class));
+                }
+            }
+        }
+
+        logOutBtn.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+
+                mAuth.signOut ();
+
+            }
+        });
 
         menuBtnWedding.setOnClickListener (new View.OnClickListener () {
             @Override
