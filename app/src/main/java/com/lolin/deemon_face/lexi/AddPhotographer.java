9bb8eb76f5b0527a8f5971e2308012975e77dc31
sandunsameera <sandunsameera25@gmail.com>
2019-01-24 17:53:57 +0530

    Adding Photographer details to firebase database

--- app/src/main/java/com/lolin/deemon_face/lexi/AddPhotographer.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/AddPhotographer.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
@@ -12,6 +14,11 @@ public class AddPhotographer extends AppCompatActivity {
 
     public Button AddPhotographyer_btn_add;
     private Firebase mref;
+    public EditText AddPhotographer_ET_name;
+    public EditText AddPhotographer_ET_Age;
+    public EditText AddPhotographer_ET_Expe;
+    public EditText AddPhotographer_ET_Email;
+    public EditText AddPhotographer_ET_phone;
 
 
     @Override
@@ -21,15 +28,32 @@ public class AddPhotographer extends AppCompatActivity {
         Firebase.setAndroidContext(this);
 
         AddPhotographyer_btn_add = findViewById (R.id.AddPhotography_btn_add);
+        AddPhotographer_ET_name = findViewById (R.id.AddPhotographer_ET_Nmae);
+        AddPhotographer_ET_Age = findViewById (R.id.AddPhotographer_ET_Age);
+        AddPhotographer_ET_Expe= findViewById (R.id.AddPhotographer_ET_Experience);
+        AddPhotographer_ET_Email= findViewById (R.id.AddPhotographer_ET_Email);
+        AddPhotographer_ET_phone= findViewById (R.id.AddPhotographer_ET_Phone);
 
+        //Creating connection to database
         mref = new Firebase ("https://lexi-750af.firebaseio.com/");
-
+//
         AddPhotographyer_btn_add.setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick(View v) {
-                Firebase mRefChild = mref.child ("Name");
+                Firebase mRefChildName = mref.child ("Name");
+                Firebase mRefChildAge= mref.child ("Age");
+                Firebase mRefChildExp= mref.child ("Experience");
+                Firebase mRefChildEmail= mref.child ("Email");
+                Firebase mRefChildphone= mref.child ("Phone");
 
-                mRefChild.setValue ("Sandun");
+                //Setting up the values
+                mRefChildName.setValue (AddPhotographer_ET_name.getText ().toString ());
+                mRefChildAge.setValue (AddPhotographer_ET_Age.getText ().toString ());
+                mRefChildExp.setValue (AddPhotographer_ET_Expe.getText ().toString ());
+                mRefChildEmail.setValue (AddPhotographer_ET_Email.getText ().toString ());
+                mRefChildphone.setValue (AddPhotographer_ET_phone.getText ().toString ());
+
+                Toast.makeText (AddPhotographer.this,"Successfully added",Toast.LENGTH_SHORT).show ();
 
             }
         });
