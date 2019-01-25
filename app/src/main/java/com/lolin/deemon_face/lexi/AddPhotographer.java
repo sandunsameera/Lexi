35fd51d85033c329a23d5a078d0ebe08a7cebb98
sandunsameera <sandunsameera25@gmail.com>
2019-01-25 09:44:13 +0530

    finishing forms and data entry to firebase db

--- app/src/main/java/com/lolin/deemon_face/lexi/AddPhotographer.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/AddPhotographer.java
@@ -1,14 +1,13 @@
 package com.lolin.deemon_face.lexi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.client.Firebase;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class AddPhotographer extends AppCompatActivity {
 
