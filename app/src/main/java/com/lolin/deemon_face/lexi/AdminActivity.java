68133589dcee696fcb36046e26dd74744aff142a
sandunsameera <sandunsameera25@gmail.com>
2019-01-25 10:23:04 +0530

    ui update

--- app/src/main/java/com/lolin/deemon_face/lexi/AdminActivity.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/AdminActivity.java
@@ -1,14 +1,12 @@
 package com.lolin.deemon_face.lexi;
 
-import android.content.ClipData;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -25,39 +23,31 @@ public class AdminActivity extends AppCompatActivity {
         admin_btn_decorating = findViewById (R.id.admin_btn_addDecorator);
         admin_btn_photography = findViewById (R.id.admin_btn_addPhotographer);
 
-        registerForContextMenu (admin_btn_caterning);
-        registerForContextMenu (admin_btn_decorating);
-        registerForContextMenu (admin_btn_photography);
+        admin_btn_photography.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent (AdminActivity.this,AddActivity.class);
+                startActivity (intent1);
+            }
+        });
+
+        admin_btn_caterning.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent (AdminActivity.this,AddActivity.class);
+                startActivity(intent2);
+            }
+        });
+
+       admin_btn_decorating.setOnClickListener (new View.OnClickListener () {
+           @Override
+           public void onClick(View v) {
+               Intent intent3 = new Intent (AdminActivity.this,AddActivity.class);
+               startActivity (intent3);
+           }
+       });
+
     }
 
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu (menu, v, menuInfo);
-        menu.setHeaderTitle ("Context Menu");
-        menu.add (0,v.getId (),0,"add");
-        menu.add (0,v.getId (),0,"delete");
-        menu.add (0,v.getId (),0,"update");
-    }
 
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-
-        if (item.getTitle () == "add"){
-            Intent intent1 = new Intent (AdminActivity.this,AddActivity.class);
-            startActivity (intent1);
-        }
-
-        else if (item.getTitle () == "delete"){
-
-            Intent intent2 = new Intent (AdminActivity.this,DeleteActivity.class);
-            startActivity (intent2);
-
-        }
-        else {
-            Intent intent3 = new Intent (AdminActivity.this,UpdateActivity.class);
-            startActivity (intent3);
-        }
-
-        return super.onContextItemSelected (item);
-    }
 }
