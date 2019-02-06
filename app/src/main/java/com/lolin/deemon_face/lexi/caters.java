2d853ea3b2d81526cf8301e0afd0686fe3f5b2d7
sandunsameera <sandunsameera25@gmail.com>
2019-02-06 22:49:18 +0530

    Use of recycler View

--- app/src/main/java/com/lolin/deemon_face/lexi/caters.java
+++ app/src/main/java/com/lolin/deemon_face/lexi/caters.java
@@ -2,4 +2,36 @@ package com.lolin.deemon_face.lexi;
 
 public class caters {
     public String Name,Age,Experience;
+    public caters(){
+
+    }
+
+
+    public caters(String Name,String Age,String Experience){
+        this.Name = Name;
+        this.Age = Age;
+        this.Experience = Experience;
+    }
+
+
+    public String getName (){
+        return Name;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public String getAge(){return Age;}
+    public void setAge (String Age){this.Age = Age;}
+
+    public String getExperience(){return  Experience;}
+    public void setExperience(String experience){this.Experience = experience;}
 }
+
+
+
+
+
+
+
+
